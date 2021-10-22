91903c8259717d0f3c0a60f68227bb80bec3a639
Joey Leung <jleung147@gmail.com>
2021-10-21 21:19:46 -0400

    Render Prices Implemented
    Blockchains prices are fetched and rendered from the backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
-import { Divider, Paper, Stack } from '@mui/material';
+import { Divider, Stack } from '@mui/material';
+import './App.css';
+import CryptoPaper from './components/CryptoPaper';
 
 function App() {
+
 	return (
 		<div className="App">
 			<Stack
-
+				className='app'
 				divider={<Divider flexItem />}
 				spacing={2}>
-				<Paper></Paper>
+				<CryptoPaper crypto='Bitcoin' />
+				<CryptoPaper crypto='Ethereum' />
 			</Stack>
 		</div>
 	);
--- frontend/src/components/CryptoPaper.js
+++ frontend/src/components/CryptoPaper.js
@@ -1,11 +1,65 @@
-import { Paper } from "@mui/material";
+import { Paper, Stack, IconButton } from "@mui/material";
+import { useEffect, useState } from "react";
+import './styles/CryptoPaper.css';
+import axios from "axios";
 
-function CryptoPaper() {
+function CryptoPaper(props) {
+
+    const [blockchainPrices, setBlockchainPrices] = useState([]);
+
+    useEffect(() => {
+        const source = axios.CancelToken.source();
+        let crypto = props.crypto.toLowerCase();
+        console.log(crypto);
+        const getBlockchainPrices = async () => {
+            axios.get(`http://localhost:8080/${crypto}-data`)
+                .then(result => {
+                    console.log(result.data);
+                    setBlockchainPrices(result.data);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        };
+        getBlockchainPrices();
+
+        return () => {
+            source.cancel();
+        };
+    }, []);
 
 
     return (
-        <Paper>
+        <Paper
+            className='section'
+            elevation={3}>
+            <h1>{props.crypto}</h1>
+            <div className='contents'>
+                <Paper className='site-info'>
+                    <h2>Blockchain.com</h2>
+                    <div>
+                        <label>Buy Price:</label>
+                        {blockchainPrices[0]}
+                    </div>
+                    <div>
+                        <label>Sell Price:</label>
+                        {blockchainPrices[1]}
+                    </div>
+                </Paper>
 
+                <Stack className='recommendations'>
+                    <IconButton aria-label="" disabled={true}>
+                    </IconButton>
+                    <IconButton aria-label="" disabled={true}>
+                    </IconButton>
+                </Stack>
+
+                <Paper className='site-info'>
+                    <h2>OtherSite.com</h2>
+                    <label>Buy:</label>
+                    <label>Sell:</label>
+                </Paper>
+            </div>
         </Paper>
     );
 }
