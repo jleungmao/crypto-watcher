1478253463ff4b31675396fa47a89607313774d5
Joey Leung <jleung147@gmail.com>
2021-10-21 21:02:38 -0400

    Added CORS

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 
 const userRouter = require('./routes/userRoutes');
 
+//initialize the app
 const app = express();
+const corsOptions = {
+    origin: 'http://localhost:8080',
+    credentials: true,
+    optionSuccessStatus: 200
+};
 
+//set the routes, cors
+app.use(cors(corsOptions));
 app.use(userRouter);
 
+
+//start app
 app.listen(8080);
