43dfb5876039095221cef3274b1899ae49039182
Joey Leung <jleung147@gmail.com>
2021-10-26 22:47:54 -0400

    Added display for buy/sell prices

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,47 @@
-import { Divider, Stack } from '@mui/material';
+import { Divider, getStepLabelUtilityClass, Stack } from '@mui/material';
 import './App.css';
 import CryptoPaper from './components/CryptoPaper';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+	const [allPrices, setAllPrices] = useState({
+		bitcoin: {},
+		ethereum: {}
+	});
+	const websites = ['blockchain', 'bittrex'];
+	const cryptos = ['bitcoin', 'ethereum'];
+
+	useEffect(() => {
+		const source = axios.CancelToken.source();
+		const getPriceData = async () => {
+			for (const site of websites) {
+				axios.get(`http://localhost:8080/${site}-data`)
+					.then(result => {
+						let temp = { ...allPrices };
+						for (const crypto of cryptos) {
+							temp[crypto][site] = result.data[crypto];
+						}
+						setAllPrices(temp);
+					})
+					.catch(err => {
+						console.log(err);
+					});
+			}
+		};
+
+		getPriceData();
+
+		return () => {
+			source.cancel();
+		};
+	}, []);
+
+	useEffect(() => {
+		console.log(allPrices);
+	}, [allPrices]);
+
 
 	return (
 		<div className="App">
@@ -10,8 +49,9 @@ function App() {
 				className='app'
 				divider={<Divider flexItem />}
 				spacing={2}>
-				<CryptoPaper crypto='Bitcoin' />
-				<CryptoPaper crypto='Ethereum' />
+				<CryptoPaper crypto='Bitcoin' prices={allPrices.bitcoin} />
+
+				<CryptoPaper crypto='Ethereum' prices={allPrices.ethereum} />
 			</Stack>
 		</div>
 	);
--- frontend/src/components/CryptoPaper.js
+++ frontend/src/components/CryptoPaper.js
@@ -1,36 +1,13 @@
 import { Paper, Stack, IconButton } from "@mui/material";
 import { useEffect, useState } from "react";
 import './styles/CryptoPaper.css';
-import axios from "axios";
 
 function CryptoPaper(props) {
-
-    const [blockchainPrices, setBlockchainPrices] = useState([]);
-
-    useEffect(() => {
-        const source = axios.CancelToken.source();
-        let crypto = props.crypto.toLowerCase();
-        console.log(crypto);
-        const getBlockchainPrices = async () => {
-            axios.get(`http://localhost:8080/${crypto}-data`)
-                .then(result => {
-                    console.log(result.data);
-                    setBlockchainPrices(result.data);
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-        };
-        getBlockchainPrices();
-
-        return () => {
-            source.cancel();
-        };
-    }, []);
 
 
     return (
         <Paper
+            key={props.prices}
             className='section'
             elevation={3}>
             <h1>{props.crypto}</h1>
@@ -39,11 +16,11 @@ function CryptoPaper(props) {
                     <h2>Blockchain.com</h2>
                     <div>
                         <label>Buy Price:</label>
-                        {blockchainPrices[0]}
+                        {props.prices && props.prices.blockchain ? props.prices.blockchain.buy : '...'}
                     </div>
                     <div>
                         <label>Sell Price:</label>
-                        {blockchainPrices[1]}
+                        {props.prices && props.prices.blockchain ? props.prices.blockchain.buy : '...'}
                     </div>
                 </Paper>
 
@@ -55,9 +32,15 @@ function CryptoPaper(props) {
                 </Stack>
 
                 <Paper className='site-info'>
-                    <h2>OtherSite.com</h2>
-                    <label>Buy:</label>
-                    <label>Sell:</label>
+                    <h2>Bittrex.com</h2>
+                    <div>
+                        <label>Buy Price:</label>
+                        {props.prices && props.prices.bittrex ? props.prices.bittrex.buy : '...'}
+                    </div>
+                    <div>
+                        <label>Sell Price:</label>
+                        {props.prices && props.prices.bittrex ? props.prices.bittrex.sell : '...'}
+                    </div>
                 </Paper>
             </div>
         </Paper>
